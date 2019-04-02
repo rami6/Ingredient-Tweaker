95a8dddea78d2031611055c35c79b9fabcf0cde9
rami6 <30137645+rami6@users.noreply.github.com>
2019-04-01 22:54:57 -0700

    Updates

---
+++ precache-manifest.3d76824a661140e65cf0daab2e9547f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cc018decdb9b4fbf4b8",
+    "url": "/Ingredient-Tweaker/static/css/main.bc0e1296.chunk.css"
+  },
+  {
+    "revision": "1cc018decdb9b4fbf4b8",
+    "url": "/Ingredient-Tweaker/static/js/main.599cfe68.chunk.js"
+  },
+  {
+    "revision": "1cfd429ca04f4b4aaeb9",
+    "url": "/Ingredient-Tweaker/static/js/runtime~main.aee883a2.js"
+  },
+  {
+    "revision": "1ae5f1f40c9a3ea5d74a",
+    "url": "/Ingredient-Tweaker/static/js/2.c43d9815.chunk.js"
+  },
+  {
+    "revision": "7a45366e98c6a035f4ced50a76367370",
+    "url": "/Ingredient-Tweaker/index.html"
+  }
+];
